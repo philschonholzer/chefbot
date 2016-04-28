49ecb7c40ec4d8b740334d1c93bbf5fab45f7c99
philschonholzer <philip.schoenholzer@apptiva.ch>
2016-04-28 21:36:12 +0200

    storage in typescript

--- src/redis_storage.ts
+++ src/redis_storage.ts
@@ -6,7 +6,78 @@ interface Config {
     host?: string;
     port?: string;
     methods?: string;
+    auth_pass?: string;
 }
+
+/**
+ * Store
+ */
+class Store {
+
+    constructor(private hash: string, private client: redis.RedisClient, private config: Config) {
+        this.hash = hash;
+        this.client = client;
+    }
+    
+    public get = (id: string, cb: (err: any, obj: any) => void) => {
+                    this.client.hget(this.config.namespace + ":" + this.hash, id, (err, res) => cb(err, JSON.parse(res)) );
+                };
+    
+    public save = (object, cb) => {
+                    if (!object.id) // Silently catch this error?
+                        return cb(new Error("The given object must have an id property"), {});
+                    this.client.hset(this.config.namespace + ":" + this.hash, object.id, JSON.stringify(object), cb);
+                };
+    public all = (cb, options) => {
+                    this.client.hgetall(this.config.namespace + ":" + this.hash, function(err, res) {
+                        if (err)
+                        return cb(err, {});
+
+                        if (null === res)
+                        return cb(err, res);
+
+                        let parsed;
+                        let array = [];
+
+                        for (let i in res) {
+                            parsed = JSON.parse(res[i]);
+                            res[i] = parsed;
+                            array.push(parsed);
+                        }
+
+                        cb(err, options && options.type === "object" ? res : array);
+                    });
+                };
+     public allById = (cb) => this.all(cb, {type: "object"});
+    
+}
+
+/**
+ * Storage
+ */
+export default class Storage {
+
+    // this.client is set to late...?
+    public teams = new Store("teams", this.client, this.config);
+    public users = new Store("users", this.client, this.config);
+    public channels = new Store("channels", this.client, this.config);
+    public projects = new Store("projects", this.client, this.config);
+
+    private client: redis.RedisClient;
+
+    constructor(private config?: Config) {
+        this.config = config || {};
+        this.config.namespace = this.config.namespace || "botkit:store";
+
+        this.client = redis.createClient(config); // could pass specific redis config here
+    }
+
+}
+
+// module.exports = (config: Config): Storage => {
+//     return new Storage(config);
+// }
+
 /*
  * All optional
  *
@@ -19,7 +90,9 @@ interface Config {
  * https://github.com/NodeRedis/node_redis
  * #options-is-an-object-with-the-following-possible-properties for a full list of the valid options
  */
-module.exports = function(config: Config) {
+
+/*
+module.exports = (config: Config) => {
     config = config || {};
     config.namespace = config.namespace || "botkit:store";
 
@@ -31,17 +104,15 @@ module.exports = function(config: Config) {
     for (let i = 0; i < methods.length; i++) {
         storage[methods[i]] = function(hash) {
             return {
-                get: function(id, cb) {
-                    client.hget(config.namespace + ":" + hash, id, function(err, res) {
-                        cb(err, JSON.parse(res));
-                    });
+                get: (id: string, cb: (err: any, obj: any) => void) => {
+                    client.hget(config.namespace + ":" + hash, id, (err, res) => cb(err, JSON.parse(res)) );
                 },
-                save: function(object, cb) {
+                save: (object, cb) => {
                     if (!object.id) // Silently catch this error?
                         return cb(new Error("The given object must have an id property"), {});
                     client.hset(config.namespace + ":" + hash, object.id, JSON.stringify(object), cb);
                 },
-                all: function(cb, options) {
+                all: (cb, options) => {
                     client.hgetall(config.namespace + ":" + hash, function(err, res) {
                         if (err)
                         return cb(err, {});
@@ -61,12 +132,10 @@ module.exports = function(config: Config) {
                         cb(err, options && options.type === "object" ? res : array);
                     });
                 },
-                allById: function(cb) {
-                    this.all(cb, {type: "object"});
-                }
+                allById: (cb) => this.all(cb, {type: "object"})
             };
         }(methods[i]);
     }
     return storage;
 };
-
+*/
